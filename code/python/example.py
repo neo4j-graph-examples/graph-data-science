f417fe1be705ccbec6861c5ad8a9a47cbd631321
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:12:09 +0000

    Updating sandbox graph-data-science

---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (c:Person{name:$name})-[r:INTERACTS]->(other)
+RETURN other.name as person
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      name="Jaime Lannister").data())
+
+  for record in results:
+    print(record['person'])
+
+driver.close()
